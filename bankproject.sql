c4d2d7bcf6fe5f6896c1fd0d9b6afaa70b4d20a9
suman <sumanghosh8767@gmail.com>
2025-02-19 12:07:10 +0530

    sql_2nd_correction

--- bankproject.sql
+++ bankproject.sql
@@ -19,12 +19,6 @@ CREATE TABLE customer (
     status VARCHAR(20)
 );
 
-INSERT INTO customer (acno, name, address, phone, email, aadhar_no, acc_type, status, balance) VALUES
-(1, 'riya singhal', 'cf-4 surya nagar', '987181818', 'support@gmail.com', '1234-1243-4545', 'saving', 'active', 12200.00),
-(2, 'mansi goyal', 'A-75-B Brij Vihar', '9673434318', 'raju@gmail.com', '4545-1243-4545', 'current', 'active', 10000.00),
-(3, 'archana', 'cf04', '4545456', 'archana@gmail.com', '1234-5656-4545', 'saving', 'active', 10000.00),
-(7, 'Veronica', 'J-50', '112233', 'a@gmail.com', '123456', 'saving', 'active', 19500.00);
-
 CREATE TABLE transaction (
     tid INT AUTO_INCREMENT PRIMARY KEY,
     acno INT,
@@ -33,10 +27,3 @@ CREATE TABLE transaction (
     type VARCHAR(20),
     FOREIGN KEY (acno) REFERENCES customer(acno)
 );
-
-INSERT INTO transaction (tid, dot, amount, type, acno) VALUES
-(1, '2022-10-16', 2000, 'deposit', 1),
-(2, '2022-10-15', 2000, 'deposit', 2),
-(3, '2022-10-16', 1200, 'withdraw', 7),
-(4, '2022-09-26', 10000, 'deposit', 7),
-(5, '2022-09-26', 5000, 'withdraw', 7);
